f434d8904446844eb5eb9a01d52988522126ce68
Touseef90 <touseef.ghazanfar@gmail.com>
2024-02-07 07:57:40 +0500

    Lecture 20 Response Types. Done

---
+++ MagicVilla_WebAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,37 @@
+﻿using MagicVilla_WebAPI.Data;
+using MagicVilla_WebAPI.Models;
+using MagicVilla_WebAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_WebAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+            return Ok(VillaStore.villaList);
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(x => x.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
+        }
+    }
+}
---
+++ MagicVilla_WebAPI/Data/VillaStore.cs
@@ -0,0 +1,12 @@
+﻿using MagicVilla_WebAPI.Models.Dto;
+
+namespace MagicVilla_WebAPI.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO> {
+            new VillaDTO { Id = 1, Name = "Touseef" },
+            new VillaDTO { Id = 2, Name = "Aarzoo" }
+        };
+    }
+}
