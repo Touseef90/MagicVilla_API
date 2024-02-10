3f9bff3aa8aee66b68960f1027f087d82b8c6be1
Touseef90 <touseef.ghazanfar@gmail.com>
2024-02-10 11:51:10 +0500

    Lecture 51. Repository CleanUp. Done

---
+++ MagicVilla_WebAPI/Repository/IRepository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using MagicVilla_WebAPI.Models;
+using System.Linq.Expressions;
+
+namespace MagicVilla_WebAPI.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null);
+        Task<T> GetAsync(Expression<Func<T, bool>> filter = null, bool tracked = true);
+        Task CreateAsync(T entity);
+        Task RemoveAsync(T entity);
+        Task SaveAsync();
+    }
+}
