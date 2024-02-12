dfa2410d16a8ee49d66b24c79a70e2140fc1d87f
Touseef90 <touseef.ghazanfar@gmail.com>
2024-02-12 07:48:47 +0500

    Lecture 60. AutoMapper. Done

---
+++ MagicVilla_Web/Models/Dto/VillaUpdateDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_Web.Models.Dto
+{
+    public class VillaUpdateDTO
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+        public string Details { get; set; }
+        [Required]
+        public double Rate { get; set; }
+        [Required]
+        public int Occupancy { get; set; }
+        [Required]
+        public int SqFt { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
+    }
+}
