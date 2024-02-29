db0e49c77f33d71447db8e9d69aba7ca1e9e3db1
Touseef90 <touseef.ghazanfar@gmail.com>
2024-02-29 07:37:16 +0500

    Lecture 66. Create villa UI. Done

--- MagicVilla_Web/Controllers/VillaController.cs
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using System.Collections.Generic;
+using AutoMapper;
 using MagicVilla_Web.Models;
 using MagicVilla_Web.Models.Dto;
 using MagicVilla_Web.Services.IServices;
@@ -30,5 +31,26 @@ namespace MagicVilla_Web.Controllers
 
             return View(list);
         }
-    }
+
+		public async Task<IActionResult> CreateVilla()
+		{
+			return View();
+		}
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateVilla(VillaCreateDTO model)
+		{
+            if (ModelState.IsValid)
+            {
+				var response = await _villaService.CreateAsync<APIResponse>(model);
+				if (response != null && response.IsSuccess)
+				{
+                    return RedirectToAction(nameof(IndexVilla));
+				}
+			}
+
+			return View(model);
+		}
+	}
 }
