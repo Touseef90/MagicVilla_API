6ee41b92dbbe2d8f53e667cc843c78968084d397
Touseef90 <touseef.ghazanfar@gmail.com>
2024-03-05 21:26:21 +0500

    Lecture 68. Update Villa Action Methods. Done

--- MagicVilla_Web/Controllers/VillaController.cs
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Reflection;
 using AutoMapper;
 using MagicVilla_Web.Models;
 using MagicVilla_Web.Models.Dto;
@@ -47,6 +48,34 @@ namespace MagicVilla_Web.Controllers
 				if (response != null && response.IsSuccess)
 				{
                     return RedirectToAction(nameof(IndexVilla));
+				}
+			}
+
+			return View(model);
+		}
+
+		public async Task<IActionResult> UpdateVilla(int villaId)
+		{
+			var response = await _villaService.GetAsync<APIResponse>(villaId);
+			if (response != null && response.IsSuccess)
+			{
+				VillaDTO model = JsonConvert.DeserializeObject<VillaDTO>(Convert.ToString(response.Result));
+				return View(_mapper.Map <VillaUpdateDTO>(model));
+			}
+
+			return NotFound();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> UpdateVilla(VillaUpdateDTO model)
+		{
+			if (ModelState.IsValid)
+			{
+				var response = await _villaService.UpdateAsync<APIResponse>(model);
+				if (response != null && response.IsSuccess)
+				{
+					return RedirectToAction(nameof(IndexVilla));
 				}
 			}
 
