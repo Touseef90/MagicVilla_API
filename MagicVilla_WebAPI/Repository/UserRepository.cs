364297c17bebb511a3527cca4d5ce4496e75369c
Touseef90 <touseef.ghazanfar@gmail.com>
2024-04-21 19:45:46 +0500

    Lecture 89. Generate Token on Successful Login. Done

--- MagicVilla_WebAPI/Repository/UserRepository.cs
+++ MagicVilla_WebAPI/Repository/UserRepository.cs
@@ -1,17 +1,23 @@
-﻿using MagicVilla_WebAPI.Data;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using MagicVilla_WebAPI.Data;
 using MagicVilla_WebAPI.Models;
 using MagicVilla_WebAPI.Models.Dto;
 using MagicVilla_WebAPI.Repository.IRepository;
+using Microsoft.IdentityModel.Tokens;
 
 namespace MagicVilla_WebAPI.Repository
 {
 	public class UserRepository : IUserRepository
 	{
 		private readonly ApplicationDBContext _db;
+		private string secretKey;
 
-        public UserRepository(ApplicationDBContext db)
+        public UserRepository(ApplicationDBContext db, IConfiguration configuration)
         {
 			_db = db;
+			secretKey = configuration.GetValue<string>("ApiSettings:Secret");
         }
 
         public bool IsUniqueUser(string username)
@@ -28,7 +34,38 @@ namespace MagicVilla_WebAPI.Repository
 
 		public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
 		{
-			throw new NotImplementedException();
+			var user = _db.LocalUsers.FirstOrDefault(u => 
+				u.UserName.ToLower() == loginRequestDTO.UserName.ToLower()
+				&& u.Password == loginRequestDTO.Password);
+
+			if (user == null)
+			{
+				return null;
+			}
+
+			// if user found generate JWT Token
+
+			var tokenHandler = new JwtSecurityTokenHandler();
+			var key = Encoding.ASCII.GetBytes(secretKey);
+
+			var tokenDescriptor = new SecurityTokenDescriptor()
+			{
+				Subject = new ClaimsIdentity(new Claim[]
+				{
+					new Claim(ClaimTypes.Name, user.Id.ToString()),
+					new Claim(ClaimTypes.Role, user.Role)
+				}),
+				Expires = DateTime.UtcNow.AddDays(7),
+				SigningCredentials = new (new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+			};
+
+			var token = tokenHandler.CreateToken(tokenDescriptor);
+			LoginResponseDTO loginResponseDTO = new LoginResponseDTO()
+			{
+				Token = tokenHandler.WriteToken(token),
+				User = user
+			};
+			return loginResponseDTO;
 		}
 
 		public async Task<LocalUser> Register(RegistrationRequestDTO registrationRequestDTO)
