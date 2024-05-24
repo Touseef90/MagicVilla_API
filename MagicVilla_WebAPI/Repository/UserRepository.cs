505d39e502d255ac0c40f55c16fa6d4863bdda40
Touseef90 <touseef.ghazanfar@gmail.com>
2024-05-24 07:26:48 +0500

    Lecture 125. Login and Registration in Web Application. Done

--- MagicVilla_WebAPI/Repository/UserRepository.cs
+++ MagicVilla_WebAPI/Repository/UserRepository.cs
@@ -66,7 +66,7 @@ namespace MagicVilla_WebAPI.Repository
 			{
 				Subject = new ClaimsIdentity(new Claim[]
 				{
-					new Claim(ClaimTypes.Name, user.Id.ToString()),
+					new Claim(ClaimTypes.Name, user.UserName.ToString()),
 					new Claim(ClaimTypes.Role, roles.FirstOrDefault())
 				}),
 				Expires = DateTime.UtcNow.AddDays(7),
@@ -78,7 +78,7 @@ namespace MagicVilla_WebAPI.Repository
 			{
 				Token = tokenHandler.WriteToken(token),
 				User = _mapper.Map<UserDTO>(user),
-				Role = roles.FirstOrDefault(),
+				//Role = roles.FirstOrDefault(),
 			};
 			return loginResponseDTO;
 		}
